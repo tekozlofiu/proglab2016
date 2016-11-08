118c164e8f7d10b21c946f14350cdabc3cee5d44
tekozlofiu <tekozlofiu@gmail.com>
2016-11-08 12:18:46 +0100

    Add files via upload

---
+++ Háromszög.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proglab20161108
+{
+    /* 
+     * Készítsünk Háromszög osztályt, amely egy háromszöghöz tárolja a három oldal hosszúságát! Legyen képes kerület, terület számítására.
+     */
+
+    class Háromszög
+    {
+        private double a, b, c;
+        
+        // Konstruktor
+        public Háromszög(int a, int b, int c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        
+        // Kerület
+        public double Kerület()
+        {
+            return a + b + c;
+        }
+
+        // Területszámítás Hérón-képlettel
+        public double Terület()
+        {
+            double s = Kerület() / 2;
+            return Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+        }
+
+        /*
+         * Egészítse ki a Háromszög osztályt egy olyan konstruktorral, 
+         * amely véletlenszerű (0-100 közötti) oldalhosszúsággal rendelkező háromszöget generál. 
+         * A keletkező háromszögnek szerkeszthetőnek kell lennie! 
+         */
+
+        private static Random rng = new Random();
+
+        // Konstruktor
+        public Háromszög()
+        {
+            do
+            {
+                a = rng.Next(0, 101);
+                b = rng.Next(0, 101);
+                c = rng.Next(0, 101);
+            }
+            while (!Szerkeszthető());
+        }
+
+        // Szerkeszthető, ha bármely oldal kisebb a másik két oldal összegénél
+        private bool Szerkeszthető()
+        {
+            return (a < b + c) && (b < a + c) && (c < b + a);
+        }
+
+        /*
+         * Egészítse ki a Háromszög osztályt úgy, hogy módosíthatóak legyenek az oldalhosszak, 
+         * de csak úgy, hogy a háromszög szerkeszthető maradjon!
+         * (Rossz oldalhossz megadása esetén maradjon meg az eredeti hossz.)
+         */
+
+        public double A
+        {
+            get { return a; }
+            set
+            {
+                // Eltároljuk az eredeti értéket.
+                double regi = a;
+
+                // Értékül adjuk az újat.
+                a = value;
+
+                // Ha az így képzett háromszög nem lenne szerkeszthető, visszatöltjük az eredeti értéket
+                if (!Szerkeszthető())
+                    a = regi;
+            }
+        }
+
+        public double B
+        {
+            get { return b; }
+            set
+            {
+                double regi = b;
+                b = value;
+                if (!Szerkeszthető())
+                    b = regi;
+            }
+        }
+
+        public double C
+        {
+            get { return c; }
+            set
+            {
+                double regi = c;
+                c = value;
+                if (!Szerkeszthető())
+                    c = regi;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proglab20161108
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/*
+            Háromszög h1 = new Háromszög(3, 4, 5);
+            Console.WriteLine("A háromszög kerülete: {0}", h1.Kerület());
+            Console.WriteLine("A háromszög területe: {0}", h1.Terület());
+
+            Háromszög h2 = new Háromszög();
+            Console.WriteLine("A háromszög kerülete: {0}", h2.Kerület());
+            Console.WriteLine("A háromszög területe: {0}", h2.Terület());
+
+            Console.WriteLine("A háromszög oldalai {0}, {1} és {2}", h1.A, h1.B, h1.C); // getter
+            h1.A = 6; // setter
+            Console.WriteLine("A háromszög oldalai {0}, {1} és {2}", h1.A, h1.B, h1.C); // getter
+
+            Console.WriteLine(h1.Kerület());
+            Console.WriteLine(h1.Terület());
+
+            Console.WriteLine("Add meg a kör középpontjának két koordinátáját és a kör sugarát!");
+            Kör k1 = new Kör(
+                double.Parse(Console.ReadLine()),
+                double.Parse(Console.ReadLine()),
+                double.Parse(Console.ReadLine())
+            );
+            Console.WriteLine("Add meg egy pont két koordinátáját!");
+            Console.WriteLine(k1.KözépponttólVettTávolság(double.Parse(Console.ReadLine()), double.Parse(Console.ReadLine())));
+            */
+            int sugár = 10;
+            int lövésekSzáma = 15;
+            int pontszám = 0;
+            int u, v;
+
+            Céltábla játék = new Céltábla(sugár);
+            for (int i = 0; i < lövésekSzáma; i++)
+            {
+                Console.WriteLine("Adj meg két koordinátát!");
+                u = int.Parse(Console.ReadLine());
+                v = int.Parse(Console.ReadLine());
+
+                Console.Clear();
+
+                if (játék.CéltáblánVanE(u, v))
+                    Console.WriteLine("Találat!");
+                else
+                    Console.WriteLine("Mellé!");
+
+                Console.WriteLine("{0} távolságra dobtál a középponttól", (int)játék.KözépponttólVettTávolság(u, v));
+
+                pontszám += játék.DobásÉrtéke(u, v);
+            }
+            Console.Clear();
+            Console.WriteLine("Vége a játéknak. {0} pontot értél el. A helyes koordináták [{1};{2}]", pontszám, játék.X, játék.Y);
+
+            Console.ReadLine();
+        }
+    }
+}
